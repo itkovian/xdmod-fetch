10fa492722777000df8d584d1bcfc706f5bab6d5
Andy Georges <itkovian@gmail.com>
2020-07-17 18:55:30 +0200

    feat: push the payload onto the processing queue

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use rdkafka::message::{BorrowedMessage, OwnedMessage};
 use rdkafka::Message;
 use std::path::{Path, PathBuf};
 use tokio::prelude::*;
+use tokio::sync::mpsc::{channel, Receiver, Sender};
 
 fn setup_logging(debug: bool, logfile: Option<&str>) -> Result<(), log::SetLoggerError> {
     let level_filter = if debug {
@@ -45,21 +46,7 @@ async fn record_borrowed_message_receipt(msg: &BorrowedMessage<'_>) {
     info!("Message received: {}", msg.offset());
 }
 
-// Emulates an expensive, synchronous computation.
-fn expensive_computation<'a>(msg: OwnedMessage) -> String {
-    info!("Starting expensive computation on message {}", msg.offset());
-    info!(
-        "Expensive computation completed on message {}",
-        msg.offset()
-    );
-    match msg.payload_view::<str>() {
-        Some(Ok(payload)) => format!("Payload len for {} is {}", payload, payload.len()),
-        Some(Err(_)) => "Message payload is not a string".to_owned(),
-        None => "No payload".to_owned(),
-    }
-}
-
-async fn fetch(bootstrap_servers: String) {
+async fn fetch(bootstrap_servers: String, sender: Sender<String>) {
     let mut cfg = ClientConfig::new()
         .set("bootstrap.servers", &bootstrap_servers)
         .set("security.protocol", "SSL")
@@ -80,6 +67,7 @@ async fn fetch(bootstrap_servers: String) {
 
     // Create the outer pipeline on the message stream.
     let stream_processor = consumer.start().try_for_each(|borrowed_message| {
+        let mut owned_sender = sender.clone();
         async move {
             // Process each message
             record_borrowed_message_receipt(&borrowed_message).await;
@@ -87,13 +75,9 @@ async fn fetch(bootstrap_servers: String) {
             // be owned in order to be sent to a separate thread.
             let owned_message = borrowed_message.detach();
             tokio::spawn(async move {
-                // The body of this block will be executed on the main thread pool,
-                // but we perform `expensive_computation` on a separate thread pool
-                // for CPU-intensive tasks via `tokio::task::spawn_blocking`.
-                let computation_result =
-                    tokio::task::spawn_blocking(|| expensive_computation(owned_message))
-                        .await
-                        .expect("failed to wait for expensive computation");
+                if let Some(Ok(payload)) = owned_message.payload_view::<str>() {
+                    owned_sender.send(payload.to_string()).await;
+                }
             });
             Ok(())
         }
@@ -102,6 +86,10 @@ async fn fetch(bootstrap_servers: String) {
     info!("Starting event loop");
     stream_processor.await.expect("stream processing failed");
     info!("Stream processing terminated");
+}
+
+async fn process_messages(receiver: Receiver<String>) {
+    todo!();
 }
 
 #[tokio::main]
@@ -118,13 +106,14 @@ async fn main() {
         )
         .get_matches();
 
-    let bootstrap_servers = matches.value_of("bootstrap.servers").unwrap();
-
     setup_logging(true, Some("/tmp/xdmod-fetch.log"));
 
-    (0..1)
-        .map(|_| tokio::spawn(fetch(bootstrap_servers.to_owned())))
-        .collect::<FuturesUnordered<_>>()
-        .for_each(|_| async { () })
-        .await
+    let bootstrap_servers = matches.value_of("bootstrap.servers").unwrap().to_owned();
+    let (mut sender, mut receiver): (Sender<String>, Receiver<String>) = channel(1000);
+
+    tokio::spawn(async move { fetch(bootstrap_servers.to_string(), sender) });
+
+    tokio::spawn(async move {
+        process_messages(receiver);
+    });
 }
